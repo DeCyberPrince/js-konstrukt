ac382f4244ced85a2b1a8eea1c05228cc8aefa06
DeCyberPrince <decyberprince@gmail.com>
2020-10-11 01:19:15 +0500

    trick with function naming

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import './styles/main.css'
 import { model } from "./model"
-import { title, text, image, columns } from "./templates"
+import { templates } from "./templates"
 
 
 const $site = document.querySelector('#site')
 
 model.forEach(block => {
-
-  let html = ''
-
-  if (block.type === 'title') {
-    html = title(block)
-  } else if (block.type === 'text') {
-    html = text(block)
-  } else if (block.type === 'columns') {
-    html = columns(block)
-  } else if (block.type === 'image') {
-    html = image(block)
+  const toHTML = templates[block.type]
+  if (toHTML) {
+    $site.insertAdjacentHTML('beforeend', toHTML(block))
   }
-
-  $site.insertAdjacentHTML('beforeend', html)
-
 })
