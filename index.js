24d4f7061e5fafd5d274f61e4095d4588a5c1aa3
DeCyberPrince <decyberprince@gmail.com>
2020-10-11 00:53:44 +0500

    added functions to generate html-parts

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const model = [
       '111111111',
       '222222222',
       '333333333'
-    ]}
+    ]},
+  { type: 'image', value: './assets/image.png' }
 ]
 
 const $site = document.querySelector('#site')
@@ -15,36 +16,52 @@ model.forEach(block => {
   let html = ''
 
   if (block.type === 'title') {
-    html = `
-    <div class="row">
-        <div class="col-sm">
-            <h1>${block.value}</h1>
-        </div>
-    </div>
-    `
+    html = title(block)
   } else if (block.type === 'text') {
-    html = `
-    <div class="row">
-        <div class="col-sm">
-            <p>${block.value}</p>
-        </div>
-    </div>
-    `
+    html = text(block)
   } else if (block.type === 'columns') {
-    let columns = block.value.map(column => {
-      return `
-      <div class="col-sm">
-          <p>${column}</p>
-      </div>
-      `
-    })
-    html = `
-      <div class="row">
-          ${columns.join('')}
-      </div>
-      `
+    html = columns(block)
+  } else if (block.type === 'image') {
+    html = image(block)
   }
 
   $site.insertAdjacentHTML('beforeend', html)
 
 })
+
+function title(block) {
+  return `
+    <div class="row">
+        <div class="col-sm">
+            <h1>${block.value}</h1>
+        </div>
+    </div>
+  `
+}
+
+function text(block) {
+  return `
+    <div class="row">
+        <div class="col-sm">
+            <p>${block.value}</p>
+        </div>
+    </div>
+  `
+}
+
+function columns(block) {
+  let columns = block.value.map(column => ` <div class="col-sm"> <p> ${column} </p> </div> `)
+  return `
+    <div class="row">
+        ${columns.join('')}
+    </div>
+  `
+}
+
+function image(block) {
+  return `
+    <div class="row">
+        <img src="${block.value}" alt="img" />
+    </div>
+  `
+}
