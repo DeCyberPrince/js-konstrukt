8994d56a3daee34aa4e15e019f81a36b79bc856f
DeCyberPrince <decyberprince@gmail.com>
2020-10-11 18:57:39 +0500

    added abstract Block class

--- src/model.js
+++ src/model.js
@@ -1,50 +1,36 @@
 import image from './assets/image.png'
+import { Block } from './classes/blocks'
 
 export const model = [
-  {
-    type: 'title',
-    value: 'Конструктор сайтов на чистом JavaScript',
-    options: {
-      tag: 'h2',
-      styles: {
-        'text-align': 'center',
-        background: 'gray',
-        color: '#fff'
-      }
+  new Block('title', 'Конструктор сайтов на чистом JavaScript', {
+    tag: 'h2',
+    styles: {
+      'text-align': 'center',
+      background: 'gray',
+      color: '#fff'
     }
-  },
-  {
-    type: 'text',
-    value: 'Here we go with some text',
-    options: {
-      tag: 'p',
-      styles: {}
-    }
-  },
-  { type: 'columns',
-    value: [
-      '111111111',
-      '222222222',
-      '333333333'
-    ],
-    options: {
-      styles: {}
-    }
-  },
-  {
-    type: 'image',
-    value: image,
-    options: {
-      styles: {
-        display: 'flex',
-        'justify-content': 'center'
-      },
-      imageStyles: {
-        width: '500px',
-        height: 'auto'
-      },
-      classes: [''],
-      alt: 'Picture'
-    }
-  }
+  }),
+  new Block('text', 'Here we go with some text', {
+    tag: 'p',
+    styles: {}
+  }),
+  new Block('columns', [
+    '111111111',
+    '222222222',
+    '333333333'
+  ], {
+    styles: {}
+  }),
+  new Block('image', image, {
+    styles: {
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    classes: [''],
+    alt: 'Picture'
+  })
 ]
