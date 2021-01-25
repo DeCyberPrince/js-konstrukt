bef15928ee4c9c93bdd2fa4168f1adb0e99d13e0
DeCyberPrince <decyberprince@gmail.com>
2021-01-25 22:07:15 +0500

    added app class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import './styles/main.css'
+import {App} from "./classes/app";
 import { model } from './model'
-import { Site } from './classes/site'
-import { Sidebar } from './classes/sidebar'
 
-
-const site = new Site('#site')
-
-site.render(model)
-
-const updateCallback = newBlock => {
-    model.push(newBlock)
-    site.render(model)
-}
-
-new Sidebar('#panel', updateCallback)
+new App(model).init()
